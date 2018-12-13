ce6fd082f98cd25c7ff0f6831baf0ed1e5017b92
Jelle Roets <jelleroets@outlook.be>
2018-12-13 13:01:08 +0100

    server returns simple state

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/state', function(req, res, next) {
+  res.json({
+    switches:[
+      {
+        id: "Switch 1",
+        state: 0
+      },
+      {
+        id: "Switch 2",
+        state: 0
+      },
+      {
+        id: "Switch 3",
+        state: 0
+      }
+    ],
+    lights:[
+      {
+        id: "Blue",
+        state: 1
+      },
+      {
+        id: "Yellow",
+        state: 0
+      },
+      {
+        id: "Groeen",
+        state: 1
+      }
+    ]
+  });
 });
+
 
 module.exports = router;
