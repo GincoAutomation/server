7092af100f8e7f4a47f417064d6b89b902effc2d
Jelle Roets <jelleroets@outlook.be>
2018-12-20 16:43:26 +0100

    remove cors from server

--- src/app.js
+++ src/app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-var cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-app.use(cors());    // enable all origins: not very safe for production!!
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
