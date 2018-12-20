0dfc3e0dcef04c286325c7f51a6df2942fce2c9d
Jelle Roets <jelleroets@outlook.be>
2018-12-20 15:04:38 +0100

    update rsync script to log output

--- bin/sync.js
+++ bin/sync.js
@@ -14,7 +14,9 @@ const sync = new Rsync()
     .exclude(['.git', '.DS_Store', 'node_modules'])
     .destination(`${config.user}@${config.hostName}:~/HomeAutomation/server`);
 
-console.log(sync.command())
-sync.execute(function(error, code, cmd) {
-    console.log('Completed synchronising code to remote');
-});
+// console.log(sync.command())
+sync.execute(
+  (error, code, cmd) => console.log('Completed synchronising code to remote'),
+  (data) => process.stdout.write(data.toString('utf8')),
+  (data) => process.stderr.write(data.toString('utf8'))
+);
