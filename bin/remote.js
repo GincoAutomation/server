4c2864fd80d968ac23ef1e5ac53d26c4b38ffaff
Jelle Roets <jelleroets@outlook.be>
2018-12-20 15:04:25 +0100

    add script to run npm commands remotely

---
+++ bin/remote.js
@@ -0,0 +1,43 @@
+const config = require('../config/config');
+
+// const args = process.argv.slice(2);
+const npmCommand = process.argv[2];
+
+// 1) using spawn
+const { spawn } = require('child_process');
+const cmd = spawn(`ssh -t ${config.user}@${config.hostName} 'cd ~/HomeAutomation/server && npm run ${npmCommand}'`, {
+  stdio: 'inherit',
+  shell: true
+});
+
+
+// 2) using 
+// const SSH2Promise = require('ssh2-promise');
+
+// var ssh = new SSH2Promise({
+//   host: config.hostName,
+//   username: config.user,
+//   agent: process.env.SSH_AUTH_SOCK,
+//   agentForward: true
+// });
+
+// (async function(){
+//   await ssh.connect();
+//   console.log("Connection established");
+//   // var data = await ssh.exec("whoami");
+//   // console.log(data);
+
+//   var socket = await ssh.spawn(`cd ~/HomeAutomation/server && npm run ${npmCommand}`, [], {pty:true});
+//   socket.on('data', (data) => {
+//     process.stdout.write(data.toString('utf8'))
+//   }).stderr.on('data', function(data) {
+//     process.stderr.write(data.toString('utf8'))
+//   })
+//   socket.on('close', (code, signal) => {
+//     ssh.close();
+//   })
+//   process.on('SIGINT', function() {
+//     socket.write('\x03');
+//   });
+// })();
+
